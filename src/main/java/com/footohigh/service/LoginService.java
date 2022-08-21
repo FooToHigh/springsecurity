d18373a023808caeb7f903ec3f6eea84f0f00ad6
FooToHigh <FooToHigh@outlook.com>
2022-08-21 08:54:15 +0800

    Revert "123"
    This reverts commit cf052aab

--- src/main/java/com/footohigh/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.footohigh.service;
-
-import com.footohigh.domain.ResponseResult;
-import com.footohigh.domain.User;
-
-public interface LoginService {
-    ResponseResult login(User user);
-
-}
